dcf4b0b297bbc069a4a086ec570809d26115d823
ukioxz <ikatember@gmail.com>
2022-05-18 10:23:47 +0300

    Add numpy and endpoint matrix for python project
    Added numpy for generation matrix
    Used bullseye docker image

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as numPy
 
 router = APIRouter()
 
@@ -6,3 +7,15 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def getMatrix():
+    matrix1 = numPy.random.rand(10,10)
+    matrix2 = numPy.random.rand(10,10)
+    product = numPy.matmul(matrix1, matrix2)
+
+    return {
+        'matrix1': matrix1.tolist(),
+        'matrix2': matrix2.tolist(),
+        'product': product.tolist()
+    }
